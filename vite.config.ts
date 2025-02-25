e0fe832efcaa2c7d17bd28bdc703ce3c0492ac44
Littlestar0508 <rist9860@gmail.com>
2025-02-26 00:23:03 +0900

    [INIT] : alias 경로 재설정 및 tsconfig 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
-import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
-import { defineConfig } from "vite";
-import { reactClickToComponent } from "vite-plugin-react-click-to-component";
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
 
 const viteConfig = defineConfig((env) => {
-  const isDevMode = env.mode.includes("dev");
+  const isDevMode = env.mode.includes('dev');
   return {
-    base: isDevMode ? "/" : "/1Piece/",
+    base: isDevMode ? '/' : '/1Piece/',
     plugins: [
       react({
-        jsxRuntime: "automatic",
+        jsxRuntime: 'automatic',
       }),
       reactClickToComponent(),
     ],
     server: {
-      host: "localhost",
+      host: 'localhost',
       port: 3000,
       cors: true,
     },
     preview: {
-      host: "localhost",
+      host: 'localhost',
       port: 8080,
       cors: true,
     },
     resolve: {
       alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        '@': resolve(__dirname, 'src'),
       },
     },
   };
