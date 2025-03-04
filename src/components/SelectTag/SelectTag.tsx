94752875b49554820fcdebedbde80feff3ae7c0f
Littlestar0508 <rist9860@gmail.com>
2025-03-04 22:08:44 +0900

    [FEAT] : 클릭 시 상태를 업데이트하는 로직 구현
    - 아이템은 현재 더미 데이터로 임시적으로 할당
    - 객체로 이루어진 배열의 형태
    - id를 과목명으로 할당하고 이후 클릭 시 id와 subject를 비교하며 일치여부 확인 후 state 변경

--- src/components/SelectTag/SelectTag.tsx
+++ src/components/SelectTag/SelectTag.tsx
@@ -1,24 +1,65 @@
+import { useState } from 'react';
 import S from './SelectTag.module.css';
 import Tag from './Tag';
 
-const dummy: Array<string> = [
-  '국어',
-  '영어',
-  '수학',
-  '사회',
-  '과학',
-  '역사',
-  '음악',
-  '체육',
-  '도덕',
+export type DummyKey =
+  | '국어'
+  | '영어'
+  | '수학'
+  | '사회'
+  | '과학'
+  | '역사'
+  | '음악'
+  | '체육'
+  | '도덕';
+
+type DummyItemType = {
+  id: DummyKey;
+  state: boolean;
+};
+
+const dummy: DummyItemType[] = [
+  { id: '국어', state: false },
+  { id: '영어', state: false },
+  { id: '수학', state: false },
+  { id: '사회', state: false },
+  { id: '과학', state: false },
+  { id: '역사', state: false },
+  { id: '음악', state: false },
+  { id: '체육', state: false },
+  { id: '도덕', state: false },
 ];
 
 function SelectTag() {
+  const [selectedTag, setSelectedTag] = useState(dummy);
+
+  const handleUpdateTagList = (subject: DummyKey, newState: boolean) => {
+    const nextSelectedTag = selectedTag.map((item, idx) => {
+      if (item['id'] === subject) {
+        return {
+          ...item,
+          state: newState,
+        };
+      } else {
+        return item;
+      }
+    });
+
+    setSelectedTag(nextSelectedTag);
+  };
+
   return (
     <div className={S.tagContainer}>
-      {dummy.map((index) => {
-        return <Tag name={index} />;
-      })}
+      {selectedTag.map((item) => (
+        <Tag
+          subject={item['id']}
+          selected={item['state']}
+          key={item['id']}
+          onUpdate={handleUpdateTagList}
+        >
+          {item['id']}
+        </Tag>
+      ))}
     </div>
   );
 }
--- src/components/SelectTag/Tag.tsx
+++ src/components/SelectTag/Tag.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import S from './SelectTag.module.css';
+import { DummyKey } from './SelectTag';
 
 type TagProps = React.ComponentProps<'button'> & {
-  name: string;
+  selected: boolean;
+  subject: DummyKey;
+  onUpdate: (subject: DummyKey, newState: boolean) => void;
 };
 
-function Tag({ name, ...restProps }: TagProps) {
+function Tag({
+  selected,
+  subject,
+  onUpdate,
+  children,
+  ...restProps
+}: TagProps) {
+  const [pick, setPick] = useState(selected);
+
+  const handleChangeState = () => {
+    const nextPick = !pick;
+    setPick(nextPick);
+    onUpdate?.(subject, nextPick);
+  };
+
   return (
-    <button className={S.tagItem} {...restProps}>
-      #{name}
+    <button className={S.tagItem} onClick={handleChangeState} {...restProps}>
+      #{children}
     </button>
   );
 }
