39608122a08bdca864e3f48faacda165a7e0c640
Littlestar0508 <rist9860@gmail.com>
2025-03-05 00:21:30 +0900

    [COMMENT] : 앞으로 수정 사항과 현재 img태그에 대한 주석 추가

--- src/ProblemCard/ProblemCard.tsx
+++ src/ProblemCard/ProblemCard.tsx
@@ -8,9 +8,11 @@ type ProblemCardProps = React.ComponentProps<'img'> &
 function ProblemCard({ src, userName, children }: ProblemCardProps) {
   return (
     <div className={S.problemCardContainer}>
+      {/* 이미지의 src와 alt텍스트는 부모 컴포넌트에서 받아온다 */}
       <img src={src} alt={userName} className={S.profileImg} />
       <div className={S.problemInfoContainer}>
         {children}
+        {/* 더미 텍스트 이후 카드 태그 컴포넌트가 대체 될 예정 */}
         <div>ㅁㄴㅇㄹ</div>
       </div>
     </div>
