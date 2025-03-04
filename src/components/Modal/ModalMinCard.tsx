1b6d798c06177a023b35b75b1c05e2b517cfdaa8
blue31 <minijh98@gmail.com>
2025-03-04 22:50:35 +0900

    [FEAT] : 모달창 html 작성성

--- src/components/Modal/ModalMinCard.tsx
+++ src/components/Modal/ModalMinCard.tsx
@@ -1,7 +1,22 @@
 import S from './ModalMinCard.module.css';
 
 function ModalMinCard() {
-  return <div className={S.test}>ㅎㅇ</div>;
+  return (
+    <div className={S.modal}>
+      <div className={S.header}>
+        <p className={S.alert}>문제 생성 실패!</p>
+      </div>
+      <div className={S.content}>
+        <img
+          src="/icons/error_red_circle.svg"
+          alt="Alert Icon"
+          className={S.icon}
+        />
+        <p className={S.text}>최소 2개의 카드를 만들어야 합니다!</p>
+      </div>
+      <div className={S.button}>확인</div>
+    </div>
+  );
 }
 
 export default ModalMinCard;
