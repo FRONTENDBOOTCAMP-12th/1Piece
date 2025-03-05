876041a1942d4f00e5dab3dda617064ee9164bfb
hunzooyun <hunzooyun@gmail.com>
2025-03-05 23:26:18 +0900

    [FEAT] : hiddenLabel 추가

--- src/components/RadioQuestion/RadioQuestion.tsx
+++ src/components/RadioQuestion/RadioQuestion.tsx
@@ -1,6 +1,7 @@
 import Input from '@/components/Input/Input';
 import { useState } from 'react';
 import S from './RadioQuestion.module.css';
+import { FaCheckCircle, FaRegCircle } from 'react-icons/fa';
 
 interface RadioQuestionProps {
   options: string[];
@@ -11,23 +12,33 @@ function RadioQuestion({ options }: RadioQuestionProps) {
   return (
     <div>
       <label className={S.radioLabel}>선지</label>
-      {options.map((option, index) => (
-        <div key={index} className={S.radioQuestion}>
-          <input
-            type="radio"
-            name="correctAnswer"
-            checked={answer === option}
-            onChange={() => setAnswer(option)}
-          />
-          <Input
-            label={option}
-            type="text"
-            placeholder="보기를 입력해주세요"
-            maxLength={10}
-            hiddenLabel
-          />
-        </div>
-      ))}
+      {options.map((option, index) => {
+        const id = `radio-${index}`;
+        return (
+          <div key={id} className={S.radioQuestion}>
+            <input
+              id={id}
+              type="radio"
+              name="correctAnswer"
+              checked={answer === option}
+              onChange={() => setAnswer(option)}
+              className={S.radioHidden}
+            />
+            {answer === option ? (
+              <FaCheckCircle className={S.radioIcon} />
+            ) : (
+              <FaRegCircle className={S.radioIcon} />
+            )}
+            <Input
+              label={option}
+              type="text"
+              placeholder="보기를 입력해주세요"
+              maxLength={10}
+              hiddenLabel
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
