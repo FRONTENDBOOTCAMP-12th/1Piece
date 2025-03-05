ca1707d66db53131432d124870bf00d04866c5d3
Littlestar0508 <rist9860@gmail.com>
2025-03-05 16:13:01 +0900

    [FEAT] : 북마크의 상태도 부모의 컴포넌트에서 속성으로 받아오기

--- src/components/ProblemCard/ProblemCard.tsx
+++ src/components/ProblemCard/ProblemCard.tsx
@@ -6,15 +6,22 @@ type ProblemCardProps = React.ComponentProps<'img'> &
   React.ComponentProps<'div'> & {
     userName: string;
     tags: string[];
+    checked: boolean;
   };
 
-function ProblemCard({ src, tags, userName, children }: ProblemCardProps) {
+function ProblemCard({
+  src,
+  tags,
+  userName,
+  checked,
+  children,
+}: ProblemCardProps) {
   return (
     <div className={S.problemCardContainer}>
       {/* 이미지의 src와 alt텍스트는 부모 컴포넌트에서 받아온다 */}
       <img src={src} alt={userName} className={S.profileImg} />
       <div className={S.problemInfoContainer}>
-        {children}
+        <p className={S.problemTitle}>{children}</p>
         {/* 더미 텍스트 이후 카드 태그 컴포넌트가 대체 될 예정 */}
         <div className={S.tagContainer}>
           {tags.map((item) => (
@@ -22,7 +29,7 @@ function ProblemCard({ src, tags, userName, children }: ProblemCardProps) {
           ))}
         </div>
       </div>
-      <BookMark checked={false} />
+      <BookMark checked={checked} />
     </div>
   );
 }
