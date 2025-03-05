1d6a69d2d4fb7733a8bebb6fe54315693d2d437b
nurungzzy <duswl0801@khu.ac.kr>
2025-03-05 11:12:49 +0900

    [FEAT] : active 상태 관리 추가
    - 클릭 시(active) 버튼 색상이 변경되도록 구현

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,18 @@
+import { useState } from 'react';
 import S from './Button.module.css';
 
-interface ButtonProps {
-  label: string;
-  color:
+type ButtonProps = React.ComponentProps<'button'> & {
+  label?: string;
+  color?:
     | 'gray'
     | 'dark-gray'
     | 'primary'
     | 'primary-2'
     | 'secondary'
     | 'tertiary';
-  disabled: boolean;
+  disabled?: boolean;
   onClick?: () => void;
-}
+};
 
 function Button({
   label,
@@ -22,12 +23,29 @@ function Button({
   const buttonColor = `var(--${color}-g)`;
   const buttonColorClicked = `var(--clicked-${color}-g)`;
 
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMouseDown = () => {
+    setIsActive(true);
+  };
+  const handleMouseUp = () => {
+    setIsActive(false);
+  };
+
+  const styles = {
+    background: isActive ? buttonColorClicked : buttonColor,
+  };
+
   return (
     <button
       className={S.button}
-      style={{ background: buttonColor }}
+      style={styles}
       aria-label={label}
       aria-disabled={disabled}
+      onClick={onClick}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
     >
       {label}
     </button>
