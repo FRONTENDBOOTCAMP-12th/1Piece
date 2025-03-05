828bf611903c2ec25a54e225c2f7763d8672ad2b
blue31 <minijh98@gmail.com>
2025-03-05 18:01:13 +0900

    [FEAT] : CardSwiper 파일 생성성

---
+++ src/components/CardSwiper/CardSwiper.tsx
@@ -0,0 +1,8 @@
+import ProblemCard from '../ProblemCard/ProblemCard';
+import S from './CardSwiper.module.css';
+
+function CardSwiper() {
+  return <div className={S.CardSwiperContainer}>hello</div>;
+}
+
+export default CardSwiper;
