07dbccddbebcb90a808e6e9c2e3bb00efdb39db4
Littlestar0508 <rist9860@gmail.com>
2025-03-05 10:07:41 +0900

    [FEAT] : 카드 태그 컴포넌트를 이용한 리스트 렌더링
    - 부모에서 string타입의 배열을 전달받아 태그를 리스트 렌더링

--- src/components/ProblemCard/ProblemCard.tsx
+++ src/components/ProblemCard/ProblemCard.tsx
@@ -1,11 +1,13 @@
+import ProblemCardTag from '../ProblemCardTag/ProblemCardTag';
 import S from './ProblemCard.module.css';
 
 type ProblemCardProps = React.ComponentProps<'img'> &
   React.ComponentProps<'div'> & {
     userName: string;
+    tags: string[];
   };
 
-function ProblemCard({ src, userName, children }: ProblemCardProps) {
+function ProblemCard({ src, tags, userName, children }: ProblemCardProps) {
   return (
     <div className={S.problemCardContainer}>
       {/* 이미지의 src와 alt텍스트는 부모 컴포넌트에서 받아온다 */}
@@ -13,7 +15,11 @@ function ProblemCard({ src, userName, children }: ProblemCardProps) {
       <div className={S.problemInfoContainer}>
         {children}
         {/* 더미 텍스트 이후 카드 태그 컴포넌트가 대체 될 예정 */}
-        <div>ㅁㄴㅇㄹ</div>
+        <div className={S.tagContainer}>
+          {tags.map((item) => (
+            <ProblemCardTag key={item}>{item}</ProblemCardTag>
+          ))}
+        </div>
       </div>
     </div>
   );
