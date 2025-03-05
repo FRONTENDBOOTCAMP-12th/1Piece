3e9d9ed5a6738e4638346cd0c5dc35eb12cec971
blue31 <minijh98@gmail.com>
2025-03-05 14:34:10 +0900

    [FEAT] : backdrop 제작

--- src/components/Modal/ModalMinCard.tsx
+++ src/components/Modal/ModalMinCard.tsx
@@ -9,20 +9,22 @@ function ModalMinCard() {
   };
 
   return (
-    <div className={`${S.modal} ${!isVisible ? S.close : ''}`}>
-      <div className={S.header}>
-        <p className={S.alert}>문제 생성 실패!</p>
-      </div>
-      <div className={S.content}>
-        <img
-          src="/icons/error_red_circle.svg"
-          alt="Alert Icon"
-          className={S.icon}
-        />
-        <p className={S.text}>최소 2개의 카드를 만들어야 합니다!</p>
-      </div>
-      <div className={S.button} onClick={handleClose}>
-        확인
+    <div className={`${S.backdrop} ${!isVisible ? S.close : ''}`}>
+      <div className={S.modal}>
+        <div className={S.header}>
+          <p className={S.alert}>문제 생성 실패!</p>
+        </div>
+        <div className={S.content}>
+          <img
+            src="/icons/error_red_circle.svg"
+            alt="Alert Icon"
+            className={S.icon}
+          />
+          <p className={S.text}>최소 2개의 카드를 만들어야 합니다!</p>
+        </div>
+        <div className={S.button} onClick={handleClose}>
+          확인
+        </div>
       </div>
     </div>
   );
--- src/components/Modal/ModalQuestion.tsx
+++ src/components/Modal/ModalQuestion.tsx
@@ -10,29 +10,31 @@ function ModalQuestion() {
   };
 
   return (
-    <div className={`${S.modal} ${!isVisible ? S.close : ''}`}>
-      <div className={S.header}>
-        <p className={S.alert}>문제를 푸시겠습니까?</p>
-      </div>
-      <div className={S.content}>
-        <img
-          src="/icons/error_red_circle.svg"
-          alt="Alert Icon"
-          className={S.icon}
-        />
-        <div className={S.info}>
-          {' '}
-          <p className={S.title}>문제 제목</p>
-          <ProblemCardTag>영어</ProblemCardTag>
-          <p className={S.description}>문제 상세 설명</p>
+    <div className={`${S.backdrop} ${!isVisible ? S.close : ''}`}>
+      <div className={S.modalContainer}>
+        <div className={S.header}>
+          <p className={S.alert}>문제를 푸시겠습니까?</p>
         </div>
-      </div>
-      <div className={S.buttons}>
-        <div className={S.button} onClick={handleClose}>
-          취소
+        <div className={S.content}>
+          <img
+            src="/icons/error_red_circle.svg"
+            alt="Alert Icon"
+            className={S.icon}
+          />
+          <div className={S.info}>
+            {' '}
+            <p className={S.title}>문제 제목</p>
+            <ProblemCardTag>영어</ProblemCardTag>
+            <p className={S.description}>문제 상세 설명</p>
+          </div>
         </div>
-        <div className={S.button} onClick={handleClose}>
-          풀기
+        <div className={S.buttons}>
+          <div className={S.button} onClick={handleClose}>
+            취소
+          </div>
+          <div className={S.button} onClick={handleClose}>
+            풀기
+          </div>
         </div>
       </div>
     </div>
