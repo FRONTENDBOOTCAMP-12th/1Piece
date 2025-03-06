70c9d46903d3f4d2fe75dd5c6c93459c3f629dfa
blue31 <minijh98@gmail.com>
2025-03-06 11:51:32 +0900

    [FEAT] : 스와이퍼 고정 UI 선제작

--- src/components/CardSwiper/CardSwiper.tsx
+++ src/components/CardSwiper/CardSwiper.tsx
@@ -1,8 +1,47 @@
+import React from 'react';
 import ProblemCard from '../ProblemCard/ProblemCard';
 import S from './CardSwiper.module.css';
 
-function CardSwiper() {
-  return <div className={S.CardSwiperContainer}>hello</div>;
+interface CardSwiperProps {
+  data: {
+    id: string;
+    src: string;
+    userName: string;
+    tags: string[];
+    checked: boolean;
+    problemTitle: string;
+  }[];
+}
+
+function CardSwiper({ data }: CardSwiperProps) {
+  const recommendCard = data.slice(0, 4);
+
+  return (
+    <section className={S.CardSwipderContainer}>
+      <div className={S.header}>
+        <h2>이번 주 베스트 카드</h2>
+        <button className={S.btnMoreHeader}>
+          <span>더보기</span>
+        </button>
+      </div>
+      <div className={S.swiperContainer}>
+        {recommendCard.map((card) => (
+          <ProblemCard
+            key={card.id}
+            src={card.src}
+            userName={card.userName}
+            tags={card.tags}
+            checked={card.checked}
+          >
+            {card.problemTitle}
+          </ProblemCard>
+        ))}
+        <button className={S.btnNext}>
+          <img src="../../../public/icons/btn-more-circle.svg" alt="Next" />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default CardSwiper;
