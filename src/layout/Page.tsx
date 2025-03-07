d2639713773b67343017b203f0428fa3f8381187
hunzooyun <hunzooyun@gmail.com>
2025-03-07 09:54:44 +0900

    [FEAT] : router 생성
    - page별 router 연결
    - page 별 기초 토대 생성

---
+++ src/layout/Page.tsx
@@ -0,0 +1,13 @@
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
+function CommonLayout() {
+  return (
+    <h2>
+      <Header />
+      <Footer />
+    </h2>
+  );
+}
+
+export default CommonLayout;
---
+++ src/pages/QuestionCreate/Page.tsx
@@ -0,0 +1,5 @@
+function QuestionCreatePage() {
+  return <h1>문제 생성 페이지입니다</h1>;
+}
+
+export default QuestionCreatePage;
