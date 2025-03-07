b590730128519f8fb3b39bba0e7c81d8b14a884f
Littlestar0508 <rist9860@gmail.com>
2025-03-07 16:51:02 +0900

    [FEAT] : 메인 페이지 스와이퍼와 배너 결합

--- src/pages/Main/Page.tsx
+++ src/pages/Main/Page.tsx
@@ -1,5 +1,49 @@
+import CardSwiper from '@/components/CardSwiper/CardSwiper';
+import S from './Page.module.css';
+
+const dummyData = [
+  {
+    id: '1',
+    src: 'dummy/dummy_profile.jpg',
+    userName: 'dummy',
+    tags: ['국어', '영어', '수학'],
+    checked: false,
+    problemTitle: 'cardTitle1',
+  },
+  {
+    id: '2',
+    src: 'dummy/dummy_profile.jpg',
+    userName: 'dummy2',
+    tags: ['국어', '영어', '수학'],
+    checked: false,
+    problemTitle: 'cardTitle2',
+  },
+  {
+    id: '3',
+    src: 'dummy/dummy_profile.jpg',
+    userName: 'dummy3',
+    tags: ['국어', '영어', '수학'],
+    checked: false,
+    problemTitle: 'cardTitle3',
+  },
+  {
+    id: '4',
+    src: 'dummy/dummy_profile.jpg',
+    userName: 'dummy4',
+    tags: ['국어', '영어', '수학'],
+    checked: false,
+    problemTitle: 'cardTitle4',
+  },
+];
+
 function MainPage() {
-  return <h1>메인페이지입니다</h1>;
+  return (
+    <>
+      <img src="/dummy/dummy_banner.png" alt="" className={S.mainBanner} />
+      <CardSwiper data={dummyData} />
+      <CardSwiper data={dummyData} />
+    </>
+  );
 }
 
 export default MainPage;
