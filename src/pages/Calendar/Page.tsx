6c3ea171ceddc0de16e09d4e036a1031e746fa4f
blue31 <minijh98@gmail.com>
2025-03-10 14:04:40 +0900

    [FEAT] : 탭을 달력에 추가

--- src/pages/Calendar/Page.tsx
+++ src/pages/Calendar/Page.tsx
@@ -1,12 +1,20 @@
 import MyPageDiary from '@/components/MyPageDiary/MyPageDiary';
 import Calendar from '@/components/Calendar/Calendar';
+import MyPageTab from '@/components/MyPageTab/MyPageTab';
+import S from './Page.module.css';
 
 function CalendarPage() {
+  const tabs = ['달력', '뱃지'];
+  const handleTabChange = (tab: string) => {
+    console.log(`Selected tab: ${tab}`);
+  };
+
   return (
-    <div>
+    <div className={S.CalendarPageContainer}>
       <MyPageDiary title="C A L E N D A R">
         <Calendar />
       </MyPageDiary>
+      <MyPageTab tabs={tabs} onTabChange={handleTabChange} />
     </div>
   );
 }
