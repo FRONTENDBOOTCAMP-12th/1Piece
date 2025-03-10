52238ebde7679de01ad601877549fc270346c85f
blue31 <minijh98@gmail.com>
2025-03-10 16:02:49 +0900

    [FEAT] 목록 페이지 초반 html 작업

--- src/pages/ProblemList/Page.tsx
+++ src/pages/ProblemList/Page.tsx
@@ -1,5 +1,18 @@
+import SelectTag from '@/components/SelectTag/SelectTag';
+import ProblemListContainer from './components/ProblemListContainer';
+import Pagination from '@/components/Pagination/Pagination';
+
+import S from './Page.module.css';
+
 function ProblemListPage() {
-  return <div>문제목록 페이지입니다.</div>;
+  return (
+    <div className={S.listPageContainer}>
+      <h1 className={S.title}>목록</h1>
+      <SelectTag />
+      <ProblemListContainer />
+      <Pagination />
+    </div>
+  );
 }
 
 export default ProblemListPage;
--- src/pages/ProblemList/components/ProblemListContainer.tsx
+++ src/pages/ProblemList/components/ProblemListContainer.tsx
@@ -1,5 +1,79 @@
-function ProblemListContainer() {
-  return <div>문제 목록 컨테이너 컴포넌트입니다.</div>;
+import React from 'react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Pagination, Grid } from 'swiper/modules';
+import S from './ProblemListContainer.module.css';
+import ProblemCard from '@/components/ProblemCard/ProblemCard';
+
+import 'swiper/css';
+import 'swiper/css/navigation'; // 네비게이션 기능 추가 시
+import 'swiper/css/pagination'; // 페이지네이션 기능 추가 시
+import 'swiper/css/grid';
+
+interface ProblemCardData {
+  id: string;
+  src: string;
+  userName: string;
+  tags: string[];
+  checked: boolean;
+  problemTitle: string;
 }
+
+interface CardSwiperProps {
+  data: ProblemCardData[];
+}
+
+const CustomNavigation = () => {
+  const swiper = useSwiper();
+
+  return (
+    <div className={S.navigationContainer}>
+      <button className={S.btnNext} onClick={() => swiper.slideNext()}>
+        <img src="/icons/btn-more-circle.svg" alt="Next" />
+      </button>
+    </div>
+  );
+};
+
+const ProblemListContainer: React.FC<CardSwiperProps> = ({ data }) => {
+  return (
+    <div className={S.cardSwiperContainer}>
+      <div className={S.header}>
+        <h2>이번 주 베스트 카드</h2>
+        <button className={S.btnMoreHeader}>
+          <span>더보기</span>
+        </button>
+      </div>
+      <div className={S.swiperContainer}>
+        <Swiper
+          modules={[Pagination, Grid]}
+          spaceBetween={20}
+          slidesPerView={2}
+          grid={{ rows: 2, fill: 'row' }}
+          pagination={{ clickable: true }}
+          className={S.swiper}
+        >
+          {data.map((item) => (
+            <SwiperSlide key={item.id} className={S.slide}>
+              <ProblemCard
+                src={item.src}
+                userName={item.userName}
+                tags={item.tags}
+                checked={item.checked}
+              >
+                {item.problemTitle}
+              </ProblemCard>
+            </SwiperSlide>
+          ))}
+          <SwiperSlide className={S.slide}>
+            <div className={S.btnMoreCard}>
+              <p className={S.MoreCardMessage}>클릭해서 카드 더보기</p>
+            </div>
+          </SwiperSlide>
+          <CustomNavigation />
+        </Swiper>
+      </div>
+    </div>
+  );
+};
 
 export default ProblemListContainer;
