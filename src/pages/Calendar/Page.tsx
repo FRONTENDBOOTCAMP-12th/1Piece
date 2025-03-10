cf1d6c9932533d8efabd81b3539d6855e5d25de8
blue31 <minijh98@gmail.com>
2025-03-10 11:50:27 +0900

    [FEAT] : 달력 페이지 제작

---
+++ src/pages/Calendar/Page.tsx
@@ -0,0 +1,14 @@
+import MyPageDiary from '@/components/MyPageDiary/MyPageDiary';
+import Calendar from '@/components/Calendar/Calendar';
+
+function CalendarPage() {
+  return (
+    <div>
+      <MyPageDiary title="C A L E N D A R">
+        <Calendar />
+      </MyPageDiary>
+    </div>
+  );
+}
+
+export default CalendarPage;
