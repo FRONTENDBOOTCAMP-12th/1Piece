d0a25d0e80ecdffbd431cecbe745f357852a87e7
Littlestar0508 <rist9860@gmail.com>
2025-03-10 14:43:15 +0900

    [FIX] : 배포 테스트를 위한 링크 변경

--- src/layout/Header/components/LoggedOut.tsx
+++ src/layout/Header/components/LoggedOut.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router';
 function LoggedOut() {
   return (
     // 로그인 페이지로 이동(로그인 되지 않은 상태)
-    <NavLink to="/loginPage">
+    <NavLink to="/sign-up">
       <RoundedButton color="secondary" font="pretendard">
         로그인
       </RoundedButton>
