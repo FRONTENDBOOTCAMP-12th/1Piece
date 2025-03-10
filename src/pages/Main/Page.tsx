9eb6a5e36f3244181eb32d93f232abd33f4b7bda
Littlestar0508 <rist9860@gmail.com>
2025-03-10 21:20:08 +0900

    [FEAT] : supabase 데이터 GET 통신 예제 코드

--- src/pages/Main/Page.tsx
+++ src/pages/Main/Page.tsx
@@ -1,5 +1,7 @@
 import CardSwiper from '@/components/CardSwiper/CardSwiper';
 import S from './MainPage.module.css';
+import { supabase } from '@/lib/SupabaseClient';
+import { useEffect, useState } from 'react';
 
 const data = [
   {
@@ -69,6 +71,45 @@ const data = [
 ];
 
 function MainPage() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [items, setItems] = useState([
+    {
+      id: 'cardTitle8',
+      src: '/dummy/dummy_profile.jpg',
+      userName: 'dummy',
+      tags: ['국어', '영어', '수학'],
+      checked: false,
+      problemTitle: 'cardTitle',
+    },
+  ]);
+
+  const fetchItems = async () => {
+    try {
+      let { data, error } = await supabase.from('card').select('*');
+      console.log(data);
+
+      const newData = data?.map((item) => ({
+        id: item.id,
+        src: '/dummy/dummy_profile.jpg',
+        userName: 'dummy',
+        tags: item.tags,
+        checked: false,
+        problemTitle: item.problemTitle,
+      }));
+
+      if (error) throw error;
+      if (data) setItems(newData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   return (
     <>
       <img
@@ -76,9 +117,9 @@ function MainPage() {
         alt="큐젤리란"
         className={S.mainBanner}
       />
-      <CardSwiper data={data}>지난 주 베스트</CardSwiper>
+      <CardSwiper data={items}>지난 주 베스트</CardSwiper>
       <hr className={S.line} />
-      <CardSwiper data={data}>지난 주 최다 조회수</CardSwiper>
+      <CardSwiper data={items}>지난 주 최다 조회수</CardSwiper>
     </>
   );
 }
