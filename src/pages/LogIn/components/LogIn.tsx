4925dd26b3a8e44c14a1a7b646a768b5fe8b877c
hunzooyun <hunzooyun@gmail.com>
2025-03-10 15:37:44 +0900

    [DESIGN] : 로그인 CSS 제작

--- src/pages/LogIn/components/LogIn.tsx
+++ src/pages/LogIn/components/LogIn.tsx
@@ -3,6 +3,7 @@ import Input from '@/components/Input/Input';
 import { supabase } from '@/lib/SupabaseClient';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
+import S from './LogIn.module.css';
 
 function LogInForm() {
   const [id, setId] = useState('');
@@ -39,25 +40,45 @@ function LogInForm() {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <h1>로그인</h1>
-      <Input
-        label="id"
-        type="text"
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-      />
-      <Input
-        label="pw"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <a href="/Quzelly/find-id">아이디 찾기</a> |{' '}
-      <a href="/Quzelly/find-pw">비밀번호 찾기</a>
-      {error && <div>{error}</div>}
-      <Button type="submit" label="로그인" />
-    </form>
+    <div className={S.container}>
+      <form onSubmit={handleLogin} className={S.loginForm}>
+        <h1 className={S.title}>로그인</h1>
+        <Input
+          label="id"
+          type="text"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          className={S.loginInputBox}
+        />
+        <Input
+          label="pw"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className={S.loginInputBox}
+        />
+        <div className={S.findLink}>
+          <a href="/Quzelly/find-id">아이디 찾기</a>
+          &nbsp;|&nbsp;
+          <a href="/Quzelly/find-pw">비밀번호 찾기</a>
+        </div>
+        {error && <div>{error}</div>}
+        <div className={S.buttonContainer}>
+          <Button
+            type="submit"
+            label="로그인"
+            color={'primary-2'}
+            className={S.submitButton}
+          />
+          <Button
+            type="submit"
+            label="회원가입"
+            color={'tertiary'}
+            className={S.submitButton}
+          />
+        </div>
+      </form>
+    </div>
   );
 }
 
