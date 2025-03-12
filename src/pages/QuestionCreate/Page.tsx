728d84e5ff075ce4307eed766132d17ab1a51dc4
blue31 <minijh98@gmail.com>
2025-03-12 11:40:36 +0900

    [DESIGN] : 문제 생성 페이지 인풋 컴포넌트 css 수정

--- src/pages/QuestionCreate/Page.tsx
+++ src/pages/QuestionCreate/Page.tsx
@@ -16,75 +16,76 @@ function QuestionCreatePage() {
     <div className={S.questionCreateContainer}>
       <h1 className={S.title}>문제 생성</h1>
       <div className={S.Settings}>
-        <div className={S.title}>
-          <span>문제 세트 제목</span>
-          <div className={S.inputContainer}>
-            <Input
-              label="제목"
-              name="title"
-              type="text"
-              placeholder="문제 세트 제목을 입력하세요"
-            />
-          </div>
+        <div className={S.questionTitle}>
+          <label className={S.label} htmlFor="title">
+            제목
+          </label>
+          <Input
+            label="문제 세트 제목"
+            name="title"
+            type="text"
+            placeholder="문제 세트 제목을 입력하세요"
+            hiddenLabel={true}
+          />
         </div>
         <div className={S.description}>
-          <span>문제 세트 상세 설명</span>
-          <div className={S.inputContainer}>
-            <Input
-              label="설명"
-              name="description"
-              type="text"
-              placeholder="문제 세트 상세 설명을 입력하세요"
-            />
-          </div>
+          <label className={S.label} htmlFor="description">
+            상세 설명
+          </label>
+          <Input
+            label="문제 세트 상세 설명"
+            name="description"
+            type="text"
+            placeholder="문제 세트 상세 설명을 입력하세요"
+            hiddenLabel={true}
+          />
         </div>
         <div className={S.tagSelect}>
-          <span>문제 세트 태그 선택</span>
+          <label className={S.label} htmlFor="tagSelect">
+            태그 선택
+          </label>
           <SelectTag onTagSelect={handleTagSelect} />
         </div>
       </div>
 
       <div className={S.questionContainer}>
-        <div className={S.card}>
-          <div className={S.deleteButton}>
-            <img src="/path/to/trash-icon.png" alt="Delete" />
-          </div>
-          <div className={S.question}>
-            <span>문제</span>
-            <div className={S.inputContainer}>
+        <div className={S.deleteButton}>
+          <img src="/path/to/trash-icon.png" alt="Delete" />
+        </div>
+        <div className={S.questionInfo}>
+          <div className={S.questionAnswer}>
+            <div className={S.question}>
+              <label htmlFor="question">문제</label>
               <Input
                 label="문제"
                 name="question"
                 type="text"
                 placeholder="문제를 입력하세요"
+                hiddenLabel={true}
               />
             </div>
+            <div className={S.answer}>
+              <span>선지</span>
+            </div>
           </div>
-          <div className={S.answerOption}>
-            <span>선지</span>
-            {/* Add answer options here */}
-          </div>
+
           <div className={S.answerDescription}>
-            <span>해설</span>
-            <div className={S.inputContainer}>
-              <Input
-                label="해설"
-                name="explanation"
-                type="text"
-                placeholder="해설을 입력하세요"
-              />
-            </div>
+            <label htmlFor="explanation">해설</label>
+            <Input
+              label="문제 해설"
+              name="explanation"
+              type="text"
+              placeholder="해설을 입력하세요"
+              hiddenLabel={true}
+            />
           </div>
         </div>
       </div>
+      <RoundedButton>+</RoundedButton>
 
       <div className={S.btnContainer}>
-        <Button label="등록" />
         <Button label="취소" />
-      </div>
-
-      <div className={S.roundedButton}>
-        <img src="/path/to/plus-icon.png" alt="Add" />
+        <Button label="등록" />
       </div>
     </div>
   );
