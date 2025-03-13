9e728a679df953c830aa9caa4a2aa1584f16435c
blue31 <minijh98@gmail.com>
2025-03-13 13:04:24 +0900

    [FEAT] : 글자 수 표기

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import S from './TextArea.module.css';
 
 interface TextAreaProps {
@@ -5,16 +6,41 @@ interface TextAreaProps {
   placeholder?: string;
   className?: string;
   height?: string;
+  maxLength?: number;
 }
 
-function TextArea({ name, placeholder, className, height }: TextAreaProps) {
+function TextArea({
+  name,
+  placeholder,
+  className,
+  height,
+  maxLength = 500,
+}: TextAreaProps) {
+  const [text, setText] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (event.target.value.length > maxLength) {
+      event.target.value = event.target.value.substring(0, maxLength);
+    }
+    setText(event.target.value);
+  };
+
   return (
-    <textarea
-      name={name}
-      className={`${S.textArea} ${className ?? ''}`}
-      placeholder={placeholder}
-      style={{ height: height ?? '4rem' }}
-    />
+    <div className={S.textAreaContainer}>
+      <textarea
+        name={name}
+        className={`${S.textArea} ${className ?? ''}`}
+        placeholder={placeholder}
+        style={{ height: height ?? '4rem' }}
+        maxLength={maxLength}
+        value={text}
+        onChange={handleChange}
+        aria-label={placeholder}
+      />
+      <div className={S.charCount}>
+        {text.length}/{maxLength}
+      </div>
+    </div>
   );
 }
 
