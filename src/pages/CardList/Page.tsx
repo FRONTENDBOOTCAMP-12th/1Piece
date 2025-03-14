2b219947e1a3f9d46f32d56dc63e631b5c31fbe1
hunzooyun <hunzooyun@gmail.com>
2025-03-14 13:57:05 +0900

    [MODIFY] : import 구문 수정

--- src/pages/CardList/Page.tsx
+++ src/pages/CardList/Page.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import SelectTag from '@/components/SelectTag/SelectTag';
-import ProblemListContainer from './components/CardListContainer';
+import CardListContainer from './components/CardListContainer';
 import S from './Page.module.css';
 
 function CardListPage() {
@@ -14,7 +14,7 @@ function CardListPage() {
     <div className={S.listPageContainer}>
       <h1 className={S.title}>목록</h1>
       <SelectTag onTagSelect={handleTagSelect} />
-      <ProblemListContainer selectedTags={selectedTags} />
+      <CardListContainer selectedTags={selectedTags} />
     </div>
   );
 }
