d33a53dedf5dccd08c858f69e58a9eb818f736d1
hunzooyun <hunzooyun@gmail.com>
2025-03-14 13:36:49 +0900

    [MODIFY] : route 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,52 @@
-import QuestionCreatePage from '@/pages/CardCreate/Page';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
-import FindPasswordPage from '@/pages/FindPassword/Page';
-import ProblemListPage from '@/pages/CardList/Page';
-import QuestionFixPage from '@/pages/CardEdit/Page';
 import { ErrorBoundary } from 'react-error-boundary';
-import SignUpPage from '@/pages/SignUp/Page';
-import FindIdPage from '@/pages/FindId/Page';
-import LogInPage from '@/pages/LogIn/Page';
-import MainPage from '@/pages/Main/Page';
-import CalendarPage from '@/pages/Calendar/Page';
-import BadgePage from '@/pages/Badge/Page';
-import BookmarkPage from '@/pages/Bookmark/Page';
-import RecentViewPage from '@/pages/RecentView/Page';
-import CardCollectionPage from '@/pages/CardWritten/Page';
-import CommonLayout from '@/layout/Common';
-import EditProfilePage from '@/pages/EditProfile/Page';
-import SolveProblem from './pages/QuizPlay/Page';
-import ProblemSolved from '@/pages/ProblemSolved/Page';
+import Common from '@/layout/Common';
+
+const BadgePage = lazy(() => import('@/pages/Badge/Page'));
+const BookmarkPage = lazy(() => import('@/pages/Bookmark/Page'));
+const CalendarPage = lazy(() => import('@/pages/Calendar/Page'));
+const CardCreatePage = lazy(() => import('@/pages/CardCreate/Page'));
+const CardEditPage = lazy(() => import('@/pages/CardEdit/Page'));
+const CardListPage = lazy(() => import('@/pages/CardList/Page'));
+const CardWrittenPage = lazy(() => import('@/pages/CardWritten/Page'));
+const EditProfilePage = lazy(() => import('@/pages/EditProfile/Page'));
+const FindIdPage = lazy(() => import('@/pages/FindId/Page'));
+const FindPasswordPage = lazy(() => import('@/pages/FindPassword/Page'));
+const LogInPage = lazy(() => import('@/pages/LogIn/Page'));
+const MainPage = lazy(() => import('@/pages/Main/Page'));
+const QuizCompletePage = lazy(() => import('@/pages/QuizComplete/Page'));
+const QuizPlayPage = lazy(() => import('@/pages/QuizPlay/Page'));
+const RecentViewPage = lazy(() => import('@/pages/RecentView/Page'));
+const SignUpPage = lazy(() => import('@/pages/SignUp/Page'));
 
 function App() {
   return (
     <ErrorBoundary fallback={<div>에러가 발생했습니다</div>}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<CommonLayout />}>
-            <Route index element={<MainPage />} />
-            <Route path="/problem-list" element={<ProblemListPage />} />
-            <Route path="/question-create" element={<QuestionCreatePage />} />
-            <Route path="/question-fix" element={<QuestionFixPage />} />
-            <Route path="/sign-up" element={<SignUpPage />} />
-            <Route path="/login" element={<LogInPage />} />
-            <Route path="/find-id" element={<FindIdPage />} />
-            <Route path="/find-pw" element={<FindPasswordPage />} />
-            <Route path="/calendar" element={<CalendarPage />} />
-            <Route path="/edit-profile" element={<EditProfilePage />} />
-            <Route path="/badge" element={<BadgePage />} />
-            <Route path="/bookmark" element={<BookmarkPage />} />
-            <Route path="/recent-view" element={<RecentViewPage />} />
-            <Route path="/card-collection" element={<CardCollectionPage />} />
-            <Route path="/solve-problem" element={<SolveProblem />} />
-            <Route path="/problem-solved" element={<ProblemSolved />} />
-            <Route>
-              <Route path="/my-page" element={<div>마이페이지입니다</div>} />
+      <Suspense fallback={<div role="status">페이지 로딩 중...</div>}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Common />}>
+              <Route index element={<MainPage />} />
+              <Route path="/badge" element={<BadgePage />} />
+              <Route path="/bookmark" element={<BookmarkPage />} />
+              <Route path="/calendar" element={<CalendarPage />} />
+              <Route path="/card-create" element={<CardCreatePage />} />
+              <Route path="/card-edit" element={<CardEditPage />} />
+              <Route path="/card-list" element={<CardListPage />} />
+              <Route path="/card-written" element={<CardWrittenPage />} />
+              <Route path="/edit-profile" element={<EditProfilePage />} />
+              <Route path="/find-id" element={<FindIdPage />} />
+              <Route path="/find-password" element={<FindPasswordPage />} />
+              <Route path="/login" element={<LogInPage />} />
+              <Route path="/quiz-complete" element={<QuizCompletePage />} />
+              <Route path="/quiz-play" element={<QuizPlayPage />} />
+              <Route path="/recent-view" element={<RecentViewPage />} />
+              <Route path="/sign-up" element={<SignUpPage />} />
             </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </ErrorBoundary>
   );
 }
