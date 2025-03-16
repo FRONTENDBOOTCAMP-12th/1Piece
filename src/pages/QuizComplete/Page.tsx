883c5780f3337259eebb8be77dc67f815d65b28d
nurungzzy <duswl0801@khu.ac.kr>
2025-03-17 04:22:01 +0900

    [FEAT] : supabase에서 댓글 불러오기
    - 풀이를 완료한 퀴즈 카드 id와 대응되는 댓글 목록을 최신순으로 불러오기

--- src/pages/QuizComplete/Page.tsx
+++ src/pages/QuizComplete/Page.tsx
@@ -1,9 +1,36 @@
+import { useEffect } from 'react';
+import { supabase } from '@/lib/SupabaseClient';
+import useModalVisibleStore from '@/lib/ProblemModalState';
 import QuizResult from '@/components/QuizResult/QuizResult';
 import InputBox from './components/InputBox';
 import CommentList from '@/components/CommentList/CommentList';
 import S from './Page.module.css';
 
 function QuizCompletePage() {
+  const cardInfo = useModalVisibleStore((state) => state.cardInfo);
+
+  console.log(cardInfo.id);
+
+  const fetchItems = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('comment')
+        .select('*')
+        .eq('card_id', cardInfo.id)
+        .order('written_at', { ascending: false });
+
+      console.log('data: ', data);
+
+      if (!data) throw error;
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   return (
     <div className={S.pageContainer}>
       <QuizResult />
