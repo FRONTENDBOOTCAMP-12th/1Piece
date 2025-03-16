e4295ec229e5d81e59d55543a1aaadcaf9de13e1
nurungzzy <duswl0801@khu.ac.kr>
2025-03-17 04:56:15 +0900

    [FEAT] : 댓글 목록 컴포넌트에 렌더링
    - supabase에서 받아온 댓글을 CommentList 컴포넌트로 넘겨주어 화면에 렌더링 되도록 함

--- src/pages/QuizComplete/Page.tsx
+++ src/pages/QuizComplete/Page.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { supabase } from '@/lib/SupabaseClient';
 import useModalVisibleStore from '@/lib/ProblemModalState';
 import QuizResult from '@/components/QuizResult/QuizResult';
@@ -6,22 +6,39 @@ import InputBox from './components/InputBox';
 import CommentList from '@/components/CommentList/CommentList';
 import S from './Page.module.css';
 
-function QuizCompletePage() {
-  const cardInfo = useModalVisibleStore((state) => state.cardInfo);
+interface CommentData {
+  id: string;
+  userNickname: string;
+  userLevel: number;
+  commentedAt: string;
+  content: string;
+}
 
-  console.log(cardInfo.id);
+function QuizCompletePage() {
+  const [comments, setComments] = useState<CommentData[]>([]);
+  const cardInfo = useModalVisibleStore((state) => state.cardInfo);
 
   const fetchItems = async () => {
     try {
       const { data, error } = await supabase
         .from('comment')
-        .select('*')
+        .select('*, users(*)')
         .eq('card_id', cardInfo.id)
         .order('written_at', { ascending: false });
 
+      if (!data) throw error;
+
       console.log('data: ', data);
 
-      if (!data) throw error;
+      const newData = data.map((item) => ({
+        id: `${item.id}`,
+        userNickname: item.users.nickname,
+        userLevel: item.users.level,
+        commentedAt: item.written_at,
+        content: item.comment,
+      }));
+
+      setComments(newData);
     } catch (error) {
       console.log('error: ', error);
     }
@@ -36,7 +53,7 @@ function QuizCompletePage() {
       <QuizResult />
       <div className={S.rightSection}>
         <InputBox />
-        <CommentList />
+        <CommentList comments={comments} />
       </div>
     </div>
   );
