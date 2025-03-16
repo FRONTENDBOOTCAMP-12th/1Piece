a86fd26b8d2ef00dd42b80a9d7e43905cd19718a
Littlestar0508 <rist9860@gmail.com>
2025-03-17 03:08:05 +0900

    [FEAT] : 검색 시 카드 목록 페이지로 연결
    - queryParams에 search값을 부여

--- src/layout/Header/components/HeaderSearchBar.tsx
+++ src/layout/Header/components/HeaderSearchBar.tsx
@@ -1,10 +1,15 @@
 import { CiSearch } from 'react-icons/ci';
 import S from './HeaderSearchBar.module.css';
+import { useNavigate } from 'react-router';
 
 function HeaderSearchBar() {
+  const navigation = useNavigate();
+
   // 이후 검색하게 될 경우 일어날 이벤트 구현해야 함
-  const handleSearch = () => {
-    console.log(123);
+  const handleSearch = (formData: FormData) => {
+    const keyword = JSON.stringify(formData.get('searchKeyword'));
+
+    navigation(`/card-list/?search=${keyword}`);
   };
 
   return (
@@ -12,7 +17,12 @@ function HeaderSearchBar() {
       <label htmlFor="headerSearchBar" className="sr-only">
         검색
       </label>
-      <input type="search" id="headerSearchBar" className={S.headerSearchBar} />
+      <input
+        type="search"
+        id="headerSearchBar"
+        className={S.headerSearchBar}
+        name="searchKeyword"
+      />
       {/* submit 타입으로 action을 트리거할 수 있다 */}
       <button type="submit" className={S.headerSearchBtn}>
         <CiSearch size={24} />
