fb9528f4bc98ee14dd208ae41e5053ba93335b55
hunzooyun <hunzooyun@gmail.com>
2025-03-18 11:09:49 +0900

    [MODIFY] : try.catch 구문 추가, onChange handle이벤트로 분리

--- src/pages/LogIn/Page.tsx
+++ src/pages/LogIn/Page.tsx
@@ -11,44 +11,58 @@ import Swal from 'sweetalert2';
 function LogInPage() {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
-  const [error] = useState('');
   const navigate = useNavigate();
   const { setUserInfo } = useLoginStore();
 
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    const { data: users, error: userError } = await supabase
-      .from('users')
-      .select('email')
-      .eq('user_id', id)
-      .single();
 
-    if (userError || !users) {
-      toast.error('아이디가 존재하지 않습니다.');
-      return;
+    try {
+      const { data: users, error: userError } = await supabase
+        .from('users')
+        .select('email')
+        .eq('user_id', id)
+        .single();
+
+      if (userError || !users) {
+        toast.error('아이디가 존재하지 않습니다.');
+        return;
+      }
+
+      const email = users.email ?? '';
+
+      const { data, error: loginError } =
+        await supabase.auth.signInWithPassword({
+          email,
+          password,
+        });
+
+      if (loginError || !data.user || !data.session) {
+        toast.error('이메일이나 비밀번호가 일치하지 않습니다.');
+        return;
+      }
+
+      setUserInfo(data.user ?? null);
+
+      await Swal.fire({
+        icon: 'success',
+        title: '로그인 성공!',
+        text: '메인 페이지로 이동합니다.',
+        confirmButtonText: '확인',
+      }).then(() => {
+        navigate('/');
+      });
+    } catch (error) {
+      console.error(`로그인 오류:`, error);
+      toast.error('로그인 중 오류가 발생했습니다.');
     }
-    const email = users.email ?? '';
-
-    const { data, error: loginError } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (loginError || !data.user) {
-      toast.error('이메일이나 비밀번호가 일치하지 않습니다.');
-      return;
-    }
-
-    Swal.fire({
-      icon: 'success',
-      title: '로그인 성공!',
-      text: '메인 페이지로 이동합니다.',
-      confirmButtonText: '확인',
-    });
-
-    setUserInfo(data.user);
-
-    navigate('/');
   };
 
   return (
@@ -56,17 +70,17 @@ function LogInPage() {
       <form onSubmit={handleLogin} className={S.loginForm}>
         <h1 className={S.title}>로그인</h1>
         <Input
-          label="id"
+          label="아이디"
           type="text"
           value={id}
-          onChange={(e) => setId(e.target.value)}
+          onChange={handleIdChange}
           className={S.loginInputBox}
         />
         <Input
-          label="pw"
+          label="비밀번호"
           type="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handlePasswordChange}
           className={S.loginInputBox}
         />
         <div className={S.findLink}>
@@ -74,7 +88,6 @@ function LogInPage() {
           &nbsp;|&nbsp;
           <a href="/find-password">비밀번호 찾기</a>
         </div>
-        {error && <div>{error}</div>}
         <div className={S.buttonContainer}>
           <Button
             type="submit"
