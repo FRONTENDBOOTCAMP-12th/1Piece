ff354914213edddfd7459d8bf4ffa8aa0396bf01
blue31 <minijh98@gmail.com>
2025-03-19 10:08:46 +0900

    [FEAT] : 검색어가 검색창에 남아있도록 설정

--- src/layout/Header/components/HeaderSearchBar.tsx
+++ src/layout/Header/components/HeaderSearchBar.tsx
@@ -1,20 +1,33 @@
+import { useEffect } from 'react';
 import { CiSearch } from 'react-icons/ci';
-import S from './HeaderSearchBar.module.css';
-import { useNavigate } from 'react-router';
+import { useNavigate, useLocation } from 'react-router';
 import useSearchStore from '@/lib/SearchState';
+
+import S from './HeaderSearchBar.module.css';
 
 function HeaderSearchBar() {
   const navigation = useNavigate();
+  const location = useLocation();
+  const searchParam = useSearchStore((state) => state.searchParam);
   const setSearchParam = useSearchStore((state) => state.setSearchParam);
-
   // 이후 검색하게 될 경우 일어날 이벤트 구현해야 함
+  useEffect(() => {
+    const search = new URLSearchParams(location.search).get('search');
+    if (search) {
+      setSearchParam(search);
+    }
+  }, [location.search, setSearchParam]);
+
   const handleSearch = (formData: FormData) => {
     // formData를 받아와서 "" 를 삭제한 값을 저장
     const keyword = JSON.stringify(formData.get('searchKeyword')).slice(1, -1);
-
     // 전역 상태 저장소에 값 변경
     setSearchParam(keyword);
     navigation(`/card-list/?search=${keyword}`);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchParam(e.target.value);
   };
 
   return (
@@ -27,6 +40,8 @@ function HeaderSearchBar() {
         id="headerSearchBar"
         className={S.headerSearchBar}
         name="searchKeyword"
+        value={searchParam}
+        onChange={handleInputChange}
       />
       {/* submit 타입으로 action을 트리거할 수 있다 */}
       <button type="submit" className={S.headerSearchBtn} aria-label="검색하기">
