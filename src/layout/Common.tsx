d77b633a0f6f3e0b43ef297c028f74641ea40326
hunzooyun <hunzooyun@gmail.com>
2025-03-19 15:21:34 +0900

    [MODIFY] : h2 제거

--- src/layout/Common.tsx
+++ src/layout/Common.tsx
@@ -2,14 +2,14 @@ import { Outlet } from 'react-router';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 
-function CommonLayout() {
+function Common() {
   return (
-    <h2>
+    <>
       <Header />
       <Outlet />
       <Footer />
-    </h2>
+    </>
   );
 }
 
-export default CommonLayout;
+export default Common;
