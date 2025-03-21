8988a3df91150a6c231fdf53de312fb9aad52b87
Littlestar0508 <rist9860@gmail.com>
2025-03-22 02:10:12 +0900

    [MODIFY] : Footer 링크 겹침 이슈 해결
    - 파이널 발표 이후 링크 삭제 예정

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -5,20 +5,32 @@ const navigation = {
   community: {
     title: 'Community',
     menu: [
-      { text: 'Events', path: '/' },
-      { text: 'Blog', path: '/' },
-      { text: 'Podcast', path: '/' },
-      { text: 'Invite a friend', path: '/' },
+      {
+        text: 'Events',
+        path: 'https://github.com/FRONTENDBOOTCAMP-12th/1Piece',
+      },
+      { text: 'Blog', path: 'https://velog.io/@rist98/series/Quzelly' },
+      {
+        text: 'Podcast',
+        path: 'https://www.figma.com/design/IvyIVMx8wvyFSX2MzzTdJR/1Piece?node-id=178-2949&t=pGGr6p78u0cS7CIK-1',
+      },
+      {
+        text: 'Invite a friend',
+        path: 'https://bootcamp.likelion.net/?utm_source=google&utm_medium=search&utm_campaign=kdtall_google_search_brand_pcmo&utm_content=%EB%A9%8B%EC%9F%81%EC%9D%B4%20%EC%82%AC%EC%9E%90%20%EC%B2%98%EB%9F%BC&utm_term=new_00_brand_all_pcmo&gad_source=1&gclid=CjwKCAjwnPS-BhBxEiwAZjMF0lAlKA74lVl0FDnRYHPxvcBXSnPYllQIZpG_yBZrspJHmK2GdbNS-RoCT50QAvD_BwE',
+      },
     ],
   },
 
   socials: {
     title: 'Socials',
     menu: [
-      { text: 'Discord', path: '/' },
-      { text: 'Instagram', path: '/' },
-      { text: 'Twitter', path: '/' },
-      { text: 'Facebook', path: '/' },
+      {
+        text: 'Discord',
+        path: 'https://discordapp.com/users/365396272800202753',
+      },
+      { text: 'Instagram', path: 'https://www.instagram.com/shrnrwhd/' },
+      { text: 'Twitter', path: 'https://supabase.com/' },
+      { text: 'Facebook', path: 'https://zustand-demo.pmnd.rs/' },
     ],
   },
 };
@@ -40,9 +52,12 @@ function Footer() {
           </div>
           <div className={S.snsContainer}>
             {sns.map((sns, index) => (
-              <a key={`sns-${index}`} href="/">
-                <img src={`/icons/${sns}.svg`} alt={`${sns} 바로가기`} />
-              </a>
+              <img
+                key={`sns-${index}`}
+                src={`/icons/${sns}.svg`}
+                alt={`${sns} 바로가기`}
+                style={{ cursor: 'pointer' }}
+              />
             ))}
           </div>
         </div>
@@ -59,10 +74,10 @@ function Footer() {
         <nav className={S.footerPolicy}>
           <ul>
             <li>
-              <a href="/">Privacy & Policy</a>
+              <a href="https://ko.react.dev/">Privacy & Policy</a>
             </li>
             <li>
-              <a href="/">Terms & Condition</a>
+              <a href="https://www.typescriptlang.org/">Terms & Condition</a>
             </li>
           </ul>
         </nav>
