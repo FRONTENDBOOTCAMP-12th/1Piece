6e67484e2820ed63834238c5dee1f72a59e6f78e
Littlestar0508 <rist9860@gmail.com>
2025-03-23 02:30:11 +0900

    [FEAT] : 스켈레톤 UI 기본 토대 구현
    - 적용X
    - 적용할 기본 토대 작성

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,43 @@
+import S from './Skeleton.module.css';
+
+interface SkeletonProps {
+  row: 1 | 2;
+  col: 2 | 5 | 6;
+}
+
+function Skeleton({ row, col }: SkeletonProps) {
+  const gridName = `gridName${row}${col}`;
+
+  return (
+    <div className={`${S[`${gridName}`]}`}>
+      {new Array(row * col).fill(' ').map((_item, idx) => {
+        return (
+          <div key={idx} className={S.skeletonContainer}>
+            <div className={S.skeletonImg} />
+            <div className={S.skeletonInfoContainer}>
+              <div className={S.skeletonTitle} />
+              <div className={S.skeletonTagContainer}>
+                <div className={S.skeletonTag} />
+                <div className={S.skeletonTag} />
+                <div className={S.skeletonTag} />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <div className={S.skeletonContainer}>
+        <div className={S.skeletonImg} />
+        <div className={S.skeletonInfoContainer}>
+          <div className={S.skeletonTitle} />
+          <div className={S.skeletonTagContainer}>
+            <div className={S.skeletonTag} />
+            <div className={S.skeletonTag} />
+            <div className={S.skeletonTag} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skeleton;
